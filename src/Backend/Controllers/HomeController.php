aa97bb0a34677427eb09f92f3f76ef97f493dfad
Purwandi <free6300@gmail.com>
2017-02-22 10:01:16 +0700

    Initial commit

---
+++ src/Backend/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Story\Cms\Backend\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller 
+{
+    public function index()
+    {
+        return view('cms::backend.dashboard');
+    }
+}
---
+++ src/StoryCmsServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Story\Cms;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\AliasLoader;
+
+class StoryCmsServiceProvider extends ServiceProvider
+{
+
+    protected $namespace = 'Story\\Cms';
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([ __DIR__.'/../config/cms.php' => config_path('cms.php')]);
+        $this->loadViewsFrom(__DIR__.'/../views', 'cms');
+
+        $this->registerServices();
+    }
+
+    /**
+     * Register custom service provider
+     * 
+     * @return void
+     */
+    protected function registerServices()
+    {
+        $loader = AliasLoader::getInstance();
+
+        if (env('APP_ENV') !== 'production') {
+            
+        }
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        Route::middleware('web')
+             ->namespace($this->namespace)
+             ->group(__DIR__.'/../routes/web.php');
+
+        Route::group(
+            ['prefix' => 'backend', 'middleware' => ['web'], 'namespace' => $this->namespace . '\\Backend\\Controllers'], function() {
+            require __DIR__.'/../routes/backend.php';
+        });
+    }
+}
