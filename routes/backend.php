a900899c4c17249c66848e81c5f9f9f00c6fc89b
muhammad thobby rahmawan <muhammadtobbyrahmawan28@gmail.com>
2017-03-01 16:14:40 +0700

    CategoryController

--- routes/backend.php
+++ routes/backend.php
@@ -11,5 +11,12 @@ Route::post('auth/forgot', 'Auth\ForgotController@store');
 Route::group(['prefix' => 'cms/elements'], function() {
     Route::get('pages', 'PostController@index');
     Route::get('pages/add', 'PostController@create');
+    Route::get('category', 'CategoryController@index');
+    Route::get('category/add', 'CategoryController@create');
+    Route::post('category', 'CategoryController@store');
+    Route::get('category/{id}', 'CategoryController@edit');
+    Route::post('category/{id}', 'CategoryController@update');
+    Route::delete('category/{id}', 'CategoryController@destroy');
+
 });
 
---
+++ src/Backend/Controllers/CategoryController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Story\Cms\Backend\Controllers;
+
+class CategoryController extends Controller
+{
+    
+   public function index()
+   {
+    
+   }
+   public function create()
+   {
+    
+   }
+   public function store()
+   {
+    
+   }
+   public function edit()
+   {
+    
+   }
+   public function update()
+   {
+    
+   }
+   public function destroy()
+   {
+
+   }
+
+}
