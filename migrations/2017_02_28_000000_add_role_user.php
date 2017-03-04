fdc2baccbe0909e45a52157d636cdf07909de167
Purwandi <free6300@gmail.com>
2017-03-04 18:53:00 +0700

    Add nullable options

--- migrations/2017_02_28_000000_add_role_user.php
+++ migrations/2017_02_28_000000_add_role_user.php
@@ -14,7 +14,7 @@ class AddRoleUser extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->index();
+            $table->string('role')->nullable();
         });
     }
 
