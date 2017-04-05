c6d48148fc3c9527c91a890422f4f8e913fd99ff
Viant <golgar@users.noreply.github.com>
2017-04-05 11:42:52 +0700

    Develop (#4)
    * merge
    
    * edit locale handle
    
    * edit revision
    
    * remove double use

---
+++ src/Backend/Middleware/LocaleMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Story\Cms\Backend\Middleware;
+
+use App;
+use Closure;
+use Jenssegers\Date\Date;
+
+class LocaleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request [description]
+     * @param  \Closure $next    [description]
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $locale = $request->input('locale');
+        $locales = config()->get('translatable.locales');
+
+        if ($locale && in_array($locale, $locales)) {
+            App::setLocale($locale);
+            Date::setLocale($locale);
+        }
+
+        return $next($request);
+    }
+}
