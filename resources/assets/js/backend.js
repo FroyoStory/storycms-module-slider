c7d4b3c4bc03e55a2cf7b391504192991ed3c3ab
Purwandi <free6300@gmail.com>
2017-09-29 14:58:06 +0700

    Remove unused code

--- resources/assets/js/backend.js
+++ resources/assets/js/backend.js
@@ -3,7 +3,7 @@ import 'element-ui/lib/theme-default/index.css'
 
 import Vue from 'vue'
 import ElementUi from 'element-ui'
-import Vddl from 'vddl';
+import Vddl from 'vddl'
 import Api from './api'
 import locale from 'element-ui/lib/locale/lang/en'
 
@@ -11,11 +11,6 @@ Vue.use(ElementUi)
 Vue.use(Vddl)
 Vue.prototype.$http = Api
 
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
 
 window.Vue = Vue
-window.Bus = new Vue({name: 'Bus'})
+window.Bus = new Vue({ name: 'Bus' })
